535c542dc53e38e438b987739669ebabe03dca74
Sclipei,C.A. (Christopher) <c.a.sclipei@students.uu.nl>
2018-05-14 14:33:07 +0200

    Replace NTest_Items.cs

--- NunitTests/NTest_Items.cs
+++ NunitTests/NTest_Items.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NUnit.Framework;
 
 namespace STVRogue.GameLogic
 {
-    [TextFixture]
+    [TestFixture]
     public class NTest_Items
     {
         [Test]
