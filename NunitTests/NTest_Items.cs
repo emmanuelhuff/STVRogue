590f246004b6720d37ebc0d55f3664c99d185d63
Sclipei,C.A. (Christopher) <c.a.sclipei@students.uu.nl>
2018-05-14 14:24:37 +0200

    Uploaded the unit tests for Items class

---
+++ NunitTests/NTest_Items.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STVRogue.GameLogic
+{
+    [TextFixture]
+    public class NTest_Items
+    {
+        [Test]
+        public void NTest_use_item()
+        {
+            Player P = new Player();
+            Item x = new HealingPotion("pot1");
+            P.use(x);
+            Assert.True(x.used);
+        }
+        [Test]
+        public void NTest_use_healingPotionWithHalfHP()
+        {
+            Player p = new Player();
+            Item h = new HealingPotion("pot1");
+            p.HP = 50;
+            p.use(h);
+            Assert.True(p.HP > 50);
+            Assert.False(p.HP < 51);
+
+        }
+        [Test]
+        public void NTest_use_crystal()
+        {
+            Player p = new Player();
+            Item c = new Crystal("ruby");
+            p.use(c);
+            Assert.True(p.accelerated);
+            if (p.location is Bridge) ;
+        }
+    }
+    
+}
