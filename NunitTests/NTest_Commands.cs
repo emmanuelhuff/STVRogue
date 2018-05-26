cbd75660c65d2b79d619cb70d2ef3cfa9b4be184
jina <qqwwdj@gmail.com>
2018-05-26 02:00:22 +0200

    unit test for Command class

--- NunitTests/NTest_Commands.cs
+++ NunitTests/NTest_Commands.cs
@@ -8,10 +8,23 @@ namespace STVRogue.GameLogic
     [TestFixture()]
     public class NUnit_Commands
     {
-        [Test()]
-        public void TestCase()
+		Command command = new Command(1);
+
+        [Test]
+        public void NTest_create_commandclass()
         {
-			
+			Assert.Equals(command.commandId,1);
         }
+
+		[Test]
+		public void NTest_check_stringreturn(){
+			Assert.Equals(command.ToString(), "no-action");
+		}
+        
+		[Test]
+		public void NTest_check_userCommand(){
+			Console.Write("Press 2");
+			Assert.Equals(command.getUserCommand().commandId, 2);
+		}
     }
 }
