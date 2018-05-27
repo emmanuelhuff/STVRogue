9c7a41bc12e1914a7a0a8040d928c4e3588d1320
jina <qqwwdj@gmail.com>
2018-05-27 16:10:56 +0200

    Update NTest_Items.cs

--- NunitTests/NTest_Items.cs
+++ NunitTests/NTest_Items.cs
@@ -25,9 +25,10 @@ namespace STVRogue.GameLogic
         [Test]
         public void NTest_use_item()
         {
-            Player P = new Player();
+            Player p = new Player();
             Item x = new HealingPotion("pot1");
-            P.use(x);
+			p.bag.Add(x);
+            p.use(x);
             Assert.True(x.used);
         }
         [Test]
@@ -36,6 +37,7 @@ namespace STVRogue.GameLogic
             Player p = new Player();
             Item h = new HealingPotion("pot1");
             p.HP = 50;
+			p.bag.Add(h);
             p.use(h);
             Assert.True(p.HP > 50);
             Assert.False(p.HP < 51);
@@ -47,6 +49,7 @@ namespace STVRogue.GameLogic
 			Dungeon dungeon = new Dungeon(5, 6);
             Player p = new Player();
             Item c = new Crystal("ruby");
+			p.bag.Add(c);
             p.use(c);
 			Assert.True(p.accelerated);
 			if(p.location is Bridge)
