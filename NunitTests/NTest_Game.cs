dd573deb26566da00122a65ff0e58130441f4050
Huff,E.K. (Emmanuel) <e.k.huff@students.uu.nl>
2018-05-27 02:04:29 +0200

    Update NTest_Game.cs

--- NunitTests/NTest_Game.cs
+++ NunitTests/NTest_Game.cs
@@ -1,13 +1,52 @@
-﻿using NUnit.Framework;
+using NUnit.Framework;
+using STVRogue.Utils;
 using System;
-namespace ClassLibrary1
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STVRogue.GameLogic
 {
-    [TestFixture()]
+    [TestFixture]
     public class NTest_Game
     {
-        [Test()]
-        public void TestCase()
+        Game game = new Game(5, 5, 50);
+        [Test]
+        public void NTest_check_ifValidGame()
         {
+            Assert.IsTrue(game.predicates.isValidDungeon(game.dungeon.startNode, game.dungeon.exitNode, 5));
+        }
+
+        [Test]
+        public void NTest_check_ifLastZone()
+        {
+            Zone testZone = new Zone(5, 10);
+            //check if 5 == 5, we are last zone 
+            Assert.AreEqual(game.dungeon.difficultyLevel, testZone.id);
+        }
+
+        [Test]
+        public void NTest_not_LastZone()
+        {
+            //if not last zone, we have monsters to place still
+            Zone testZone = new Zone(5, 10);
+            //check if 5 == 5, we are last zone 
+            Assert.AreEqual(game.dungeon.difficultyLevel, testZone.id);
+
+            Assert.IsFalse(testZone.level, game.dungeon.difficultyLevel);
+
+        }
+
+        [Test]
+        public void NTest_check_ifLastZone()
+        {
+            Zone testZone = new Zone(5, 10);
+            //check if 5 == 5, we are last zone 
+            Assert.AreEqual(game.dungeon.difficultyLevel, testZone.id);
+
+            Assert.IsFalse(testZone.level, game.dungeon.difficultyLevel);
+
         }
     }
 }
