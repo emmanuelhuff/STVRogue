73c022988cc07082d065b4cdde149052da7fe158
Emmanuel Huff <e.k.huff@uu.students.nl>
2018-05-27 15:13:32 +0200

    unit testing game

--- NunitTests/NTest_Game.cs
+++ NunitTests/NTest_Game.cs
@@ -1,13 +1,87 @@
 ﻿using NUnit.Framework;
+using STVRogue.Utils;
 using System;
-namespace ClassLibrary1
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STVRogue.GameLogic
 {
-    [TestFixture()]
+    [TestFixture]
     public class NTest_Game
     {
-        [Test()]
-        public void TestCase()
+        Game game = new Game(5, 5, 50);
+
+        [Test]
+        public void NTest_check_ifValidGame()
         {
+            Assert.IsTrue(game.predicates.isValidDungeon(game.dungeon.startNode, game.dungeon.exitNode, 5));
+        }
+
+        [Test]
+        public void NTest_check_ifLastZone()
+        {
+            Zone testZone = new Zone(5, 10);
+            //check if 5 == 5, we are last zone 
+            Assert.AreEqual(game.dungeon.difficultyLevel, testZone.id);
+
+            foreach (Zone z in game.dungeon.zones)
+            {
+                if (game.dungeon.difficultyLevel != testZone.id)
+                {
+
+                }
+            }
+
+
+        }
+
+        [Test]
+        public void NTest_not_LastZone()
+        {
+            //if not last zone, we have monsters to place still
+            Zone testZone = new Zone(1, 10);
+            //check if 1 == 5, our code is not working
+            Assert.AreNotEqual(game.dungeon.difficultyLevel, testZone.id);
+
+            
+        }
+
+        [Test]
+        public void NTest_check_getProportion()
+        {
+            int test = game.getProportion(10, 5, 3);
+            Assert.AreEqual(test, 5); // 100 % 20 == 5
+        }
+
+        [Test]
+        public void NTest_check_getItemsHP()
+        {
+            int itemsHP = 0;
+            foreach (Item i in game.itemsToSeed) //for each item in the list
+            {
+                if (i.GetType() == typeof(HealingPotion)) //if item is a healing potion
+                {
+                    itemsHP += (int)((HealingPotion)i).HPvalue; //Add its HP value
+                }
+            }
+            Assert.IsTrue(itemsHP == itemsHP);
+
+        }
+
+        [Test]
+        public void NTest_check_getHPM()
+        {
+            Zone testZone = new Zone(5, 10);
+            
+        }
+
+        [Test]
+        public void NTest_check_gameCreated()
+        {
+            Zone testZone = new Zone(5, 10);
+
         }
     }
 }
