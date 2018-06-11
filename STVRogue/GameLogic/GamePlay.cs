10ecb2ca4cf1a77d8e0e961f565968f119b39c31
jina <qqwwdj@gmail.com>
2018-06-11 16:00:33 +0200

    filestream on GamePlay.cs

--- STVRogue/GameLogic/GamePlay.cs
+++ STVRogue/GameLogic/GamePlay.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using STVRogue.Utils;
+
 namespace STVRogue.GameLogic
 {
+	[Serializable]
     public class GamePlay
     {
+		BinaryFormatter formatter = new BinaryFormatter();
+
         public GamePlay()
         {
-			//GamePlay(file) load a saved game play from a file
-        }
+			
+		}
+
+		/* GamePlay(filename) load a saved game play from a file
+            Use Binary deserialize
+            Get Filename */
+		public GamePlay(String s){
+			Stream streamRead = new FileStream(s, FileMode.Open, FileAccess.Read, FileShare.None);
+			GamePlay gamePlay = (GamePlay)formatter.Deserialize(streamRead);
+			streamRead.Close();
+		}
+              
+        //** ADDED **//
+		/* save gameplay instance to file
+                Use Binary Serialize
+                needs : currentState */
+		public void save(){
+			GamePlay record = this;         
+			Stream streamWrite = new FileStream("GamePlay.bin", FileMode.Create, FileAccess.Write,
+												FileShare.None);
+			formatter.Serialize(streamWrite, record);
+			streamWrite.Close();
+		}
 
 		public void reset(){
 			//reset the recorded game play back to turn 0.
@@ -16,13 +44,12 @@ namespace STVRogue.GameLogic
 			//replay the current turn in the recorded game play
             //game play to the next turn
 		}
-
+        
 		public void getState(){
 			//to get an instace of Game representing the game's state in the game play's current turn
 		}
 
-		public Boolean replay(){
-			//replay(Specification S)
+		public Boolean replay(Specification S){
 			//replay the whole recoreded game play and test the given specificaiton S
 
 			return true;
---
+++ STVRogue/Utils/Specification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using STVRogue.GameLogic;
+
+namespace STVRogue.Utils
+{
+    public class Specification
+    {
+        public Specification()
+        {
+			
+        }
+		public bool test(Game G){
+			return G.player.HP >= 0;
+		}
+    }
+}
