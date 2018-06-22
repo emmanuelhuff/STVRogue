380b8b675b6e16e415e61429de6b930d40d0c6db
jina <qqwwdj@gmail.com>
2018-06-22 16:15:57 +0200

    add NTest_Gameplay

--- NunitTests/NTest_GamePlay.cs
+++ NunitTests/NTest_GamePlay.cs
@@ -13,12 +13,27 @@ namespace STVRogue.GameLogic
         public void test_HPplayer_never_negative()
         {
 			List<GamePlay> plays = new List<GamePlay>();
-			plays.Add(new GamePlay("test.xml"));//I can't find the location?
+			//I can't find the location?
+			plays.Add(new GamePlay("test.xml"));
 			foreach(GamePlay gp in plays){
 				//testing two "always" properties
 				Assert.IsTrue(gp.replay(new Always(Game => Game.player.HP >= 0)));
 				Assert.IsTrue(gp.replay(new Always(Game => Game.player.HP >= 0)));
 			}
+        }
+
+		[Test()]
+        public void test_HPplayer_never_negative_Unless()
+        {
+            List<GamePlay> plays = new List<GamePlay>();
+            //I can't find the location?
+            plays.Add(new GamePlay("test.xml"));
+            foreach (GamePlay gp in plays)
+            {
+				for (var m = 1; m <= 100; m++){
+					Assert.IsTrue(gp.replay(new Unless(Game => Game.activePack.members.Count == m, Game => Game.activePack.members.Count < m)));
+				}
+            }
         }
     }
 }
