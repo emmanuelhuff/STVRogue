f3627f841dd6356df70013da1078fe24074c64dd
csclipei <c.a.sclipei@students.uu.nl>
2018-06-22 17:31:10 +0200

    added specifications

---
+++ NunitTests/NTest_GameRuleSpecs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using STVRogue.GameLogic;
+using STVRogue;
+using STVRogue.Utils;
+using System.IO;
+
+
+namespace STVRogue.GameLogic
+{
+    [TestFixture]
+    class NTest_GameRuleSpecs
+    {
+
+    }
+}
--- STVRogue/Utils/Specification.cs
+++ STVRogue/Utils/Specification.cs
@@ -1,16 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using STVRogue.GameLogic;
+using System.IO;
+
 
 namespace STVRogue.Utils
 {
-    public class Specification
+    public abstract class Specification
     {
-        public Specification()
+        public abstract bool test(Game G);
+    }
+    public class Always : Specification
+    {
+        private Predicate<Game> p;
+        public Always(Predicate<Game> p) { this.p = p; }
+        public override bool test(Game G) { return p(G);  }
+    }
+    public class Unless : Specification
+    {
+        private Predicate<Game> p;
+        private Predicate<Game> q;
+        public Unless(Predicate<Game> p, Predicate<Game> q) { this.p = p; this.q = q; }
+        List<bool> history;
+        public override bool test(Game G)
         {
-			
+            bool verdict;
+            if (history.Count >= 1)
+            {
+                bool previous = history.Last();
+                verdict = !previous || (previous && (p(G) || q(G)));
+            }
+            else { verdict = true; }
+            history.Add(p(G) && !q(G));
+            return verdict;
         }
-		public bool test(Game G){
-			return G.player.HP >= 0;
-		}
     }
 }
